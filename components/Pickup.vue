cfa3ee3faec1b32bd2fd1b5053927abca1e1e768
k-arisa-911 <chororeeen@gmail.com>
2023-06-09 12:20:02 +0900

    pickupのカルーセルを調整

--- components/Pickup.vue
+++ components/Pickup.vue
@@ -1,6 +1,6 @@
 <template>
   <!--  v-appで包むとコンポーネントは正しく動作する。これはクロスブラウザーでの互換性を確保するために必要である。-->
-  <v-app class="h-80">
+  <v-app class="h-80 p-4">
     <v-carousel
       cycle
       height="320"
@@ -11,8 +11,19 @@
       <!-- 
       progress
       hide-delimiters -->
-      <v-carousel-item v-for="(slide, i) of slides" :key="i">
-        <div class="block bg-yellow-500 w-full h-full">hogehogehogeeeee</div>
+      <!-- <v-carousel-item v-for="(slide, i) of slides" :key="i">
+        <div class="block border border-primary w-full">
+          {{ slide }}
+        </div>
+      </v-carousel-item> -->
+      <v-carousel-item v-for="(blog, i) of blogs" :key="i">
+        <div class="border border-primary relative">
+          <!-- img :srcは 画像のURLを指定するための構文-->
+          <img :src="blog.eyecatch.url" class="object-cover w-80 h-80" />
+          <div class="absolute bottom-0 inset-x-0 mb-10 mx-4">
+            <p class="text-white font-bold text-4xl">{{ blog.title }}</p>
+          </div>
+        </div>
       </v-carousel-item>
     </v-carousel>
   </v-app>
@@ -25,6 +36,12 @@ export default {
     return {
       slides: ["indigo", "green", "yellow", "pink"],
     };
+  },
+  props: {
+    blogs: [],
+  },
+  mounted() {
+    console.log("pickup表示するブログ記事", this.blogs);
   },
 };
 </script>
