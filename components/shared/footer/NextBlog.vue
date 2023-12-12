f0ea49284f95e4963e657f0c9e0b8a5bf03e98bc
trikka <trikka.huma@gmail.com>
2023-12-13 00:09:24 +0900

    フッターデザインの修正

--- components/shared/footer/NextBlog.vue
+++ components/shared/footer/NextBlog.vue
@@ -3,22 +3,34 @@
     <NuxtLink
       v-if="blog"
       :to="`/blog/${blog.id}`"
-      class="cursor-pointer duration-300 group hover:text-primary-200 flex justify-between mx-4"
+      class="cursor-pointer group flex justify-between mx-4"
     >
-      <Synopsis :blog="blog" />
-      <div class="flex items-center w-fit justify-end">
-        <p>次の記事</p>
-        <span class="i-mdi-greater-than text-xl ml-1"></span>
+      <div class="flex w-fit flex-col">
+        <div>
+          <div
+            class="flex items-center flex-none text-primary-200 group-hover:text-primary duration-300"
+          >
+            <p>次の記事</p>
+            <span class="i-mdi-greater-than text-xl ml-1"></span>
+          </div>
+        </div>
+        <p
+          class="mt-4 text-3xl text-accent-200 font-bold group-hover:text-accent duration-300"
+        >
+          {{ blog.title }}
+        </p>
       </div>
+      <img
+        :src="blog.eyecatch.url"
+        class="h-28 w-40 object-cover group-hover:brightness-110 duration-300"
+      />
     </NuxtLink>
   </div>
 </template>
 
 <script>
-import Synopsis from "./Synopsis.vue";
 export default {
   name: "NextBlog",
-  components: { Synopsis },
   props: {
     blog: {},
   },
